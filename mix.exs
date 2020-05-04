d24f6b6550d82f1a37391b8c21934d065c3e6baa
Victoria <this.is.vbykova@gmail.com>
2020-05-04 17:28:35 +0300

    upd mix.exs

--- mix.exs
+++ mix.exs
@@ -33,9 +33,8 @@ defmodule Solitaire.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.12"},
+      {:phoenix, ">= 1.4.12"},
       {:phoenix_pubsub, "~> 1.1"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
