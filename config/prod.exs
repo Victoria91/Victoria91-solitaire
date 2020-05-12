960b9f35ede0a739e00ae3d138d2b8640bb1b5d3
Victoria <this.is.vbykova@gmail.com>
2020-05-12 18:43:29 +0300

    dokku

--- config/prod.exs
+++ config/prod.exs
@@ -10,8 +10,13 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :solitaire, SolitaireWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [
+    host: System.get_env("HOSTNAME"),
+    port: "80",
+    transport_options: [socket_opts: [:inet6]]
+  ],
+  cache_static_manifest: "priv/static/cache_manifest.json",
+  http: [:inet6, port: System.get_env("PORT") || 4000]
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -52,4 +57,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+# import_config "prod.secret.exs"
