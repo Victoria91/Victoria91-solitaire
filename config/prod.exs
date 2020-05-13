55db3b8bde8f5e60524ea0af55120150192ce162
Victoria <this.is.vbykova@gmail.com>
2020-05-13 15:56:35 +0300

    move to end

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :solitaire, SolitaireWeb.Endpoint,
   ],
   cache_static_manifest: "priv/static/cache_manifest.json",
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  check_origin: ["https://solitaire.dbykov.com/"]
+  check_origin: [System.get_env("ORIGIN_URL")]
 
 # Do not print debug messages in production
 config :logger, level: :info
