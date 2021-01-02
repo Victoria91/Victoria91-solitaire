e8b04bee80ae2123a097ec7c33ef17077632dbed
Victoria <this.is.vbykova@gmail.com>
2021-01-02 17:49:07 +0300

    klondike as default type

--- config/config.exs
+++ config/config.exs
@@ -28,7 +28,7 @@ config :phoenix, :json_library, Jason
 
 config :solitaire, :game,
   suit_count: 1,
-  type: :spider
+  type: :klondike
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
